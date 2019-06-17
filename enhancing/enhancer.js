ab13117e2a42ac9565545aeba3ce0e0ff1cf0d13
Sarahlee671 <sarah.lee1987@hotmail.com>
2019-06-17 15:16:58 -0700

    added fail test

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -1,27 +1,33 @@
 module.exports = {
   succeed,
-  // fail,
+  fail,
   repair,
-  // get,
+  get,
 };
 
 function succeed(item) {
-  if(item.enhancement < 20) {
+  if(item.enhancement < 20 && item.enhancement >= 0) {
     return { ...item, enhancement: item.enhancement + 1 };
   } else {
     return {...item}
   }
 }
 
-// function fail(item) {
-//   return { ...item };
-// }
-
-function repair(item) {
-
-  return { ...item, durability: 100 };
+function fail(item) {
+  if (item.enhancement >= 15 && item.enhancement <= 16) {
+    return { ...item, durability: item.durability - 10 }
+  } else if (item.enhancement > 16) {
+    return { ...item, durability: item.durability - 10, enhancement: item.enhancement - 1 }
+  } else {
+    return {...item, durability: item.durability - 5}
+  }
 }
 
-// function get(item) {
-//   return { ...item };
-// }
+
+function repair(item) {
+  if(item.durability >= 0 && item.durability <= 100) {
+    return { ...item, durability: 100 };
+  } 
+}
+
+
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,4 +1,4 @@
-const {repair, succeed} = require('./enhancer.js');
+const {repair, succeed, fail} = require('./enhancer.js');
 
 describe('enhancer.js', () => {
     describe('repair()', () => {
@@ -13,6 +13,20 @@ describe('enhancer.js', () => {
             expect(succeed({enhancement: 1}).enhancement).toBe(2);
             expect(succeed({enhancement: 20}).enhancement).toBe(20);
             expect(succeed({enhancement: 19}).enhancement).toBe(20);
+            expect(succeed({enhancement: 0}).enhancement).toBe(1);
+        })
+    })
+    describe('fail()', () => {
+        it('should decrease durability by 5', () => {
+            expect(fail({enhancement: 10, durability: 20}).durability).toBe(15)
+        })
+
+        it('should decrease durability by 10', () => {
+            expect(fail({enhancement: 15, durability: 50}).durability).toBe(40)
+        })
+
+        it('should decrease durability by 10 and decrease enhancement by 1', () => {
+            expect(fail({enhancement: 18, durability: 40})).toEqual({enhancement: 17, durability: 30})
         })
     })
 })
